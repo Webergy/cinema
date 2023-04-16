7f4eb91aacede0ae1898f30b153550f56dc6f0f0
LechMikolaj <lech_mikolaj@interia.pl>
2023-04-16 20:41:30 +0200

    Add simple page and change settings

--- cinema/cinema/settings.py
+++ cinema/cinema/settings.py
@@ -1,3 +1,5 @@
+import os
+
 """
 Django settings for cinema project.
 
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'cinema.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        "DIRS": [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +123,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
