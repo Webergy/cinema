29e02ce77b21e57d858bce71869cf77976f61b6a
dawidektr <40436561+dawidektr@users.noreply.github.com>
2023-04-17 17:12:50 +0200

    Added Navigation Drawer
    Navigation

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,4 @@
-import { Box, Button, FormControl, FormLabel, HStack, Image, InputGroup, InputRightElement, Text,Input, Link, VStack, Accordion, AccordionItem, AccordionIcon, AccordionPanel, AccordionButton, Icon  } from '@chakra-ui/react';
+import { Box, Button, FormControl, FormLabel, HStack, Image, InputGroup, InputRightElement, Text,Input, Link, VStack, Accordion, AccordionItem, AccordionIcon, AccordionPanel, AccordionButton, Icon, useDisclosure, Drawer, DrawerOverlay, DrawerContent, DrawerCloseButton,  DrawerBody, DrawerFooter, DrawerHeader, Divider  } from '@chakra-ui/react';
 import React from 'react';
 import img1 from '../images/image1.png';
 import img2 from '../images/image2.png';
@@ -7,14 +7,79 @@ import img4 from '../images/image4.png';
 
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { ChatIcon, PhoneIcon, SearchIcon } from '@chakra-ui/icons';
+import { ChatIcon, CloseIcon, HamburgerIcon, PhoneIcon, SearchIcon } from '@chakra-ui/icons';
 import { ImFacebook} from 'react-icons/im';
 import { FaInstagramSquare} from 'react-icons/fa';
 
 const Main = () => {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const btnRef = React.useRef();
+
     return (
         <>
+            <Button position={'fixed'} padding={0} zIndex={999} right={6} bottom={12} background={'#F4C600'} w={14} h={14} borderRadius={'100%'} ref={btnRef} variantColor="teal" onClick={onOpen}>
+                <HamburgerIcon w={8} h={8}/>
+            </Button>
             <HStack >
+                
+                <Drawer
+                    isOpen={isOpen}
+                    placement="right"
+                    size={'full'}
+                    onClose={onClose}
+                    finalFocusRef={btnRef}
+                >
+                    <DrawerOverlay />
+                    <DrawerContent ps={'64px'} pe={'64px'} color={'#F4C600'} background={'#181C25'}>
+                        <DrawerHeader ps={0} pe={0} pt={'118px'}>
+                            <Text size={'lg'}>See.it</Text>
+                            <Divider borderColor={'#F4C600'} pt={8} />
+                        </DrawerHeader>
+
+                        <DrawerBody ps={0} pe={0} >
+                            <HStack mt={8} size='md'>
+                                <Icon as={FaInstagramSquare} background={'#6A45BE'} w={6} h={6} me={4}/> 
+                                <Text>Main</Text>
+                            </HStack>
+
+                            <HStack mt={8} size='md'>
+                                <Icon as={FaInstagramSquare} background={'#6A45BE'} w={6} h={6} me={4}/> 
+                                <Text>Repetitory</Text>
+                            </HStack>
+
+                            <HStack mt={8} size='md'>
+                                <Icon as={FaInstagramSquare} background={'#6A45BE'} w={6} h={6} me={4}/> 
+                                <Text>Events</Text>
+                            </HStack>
+
+                            <HStack mt={8} size='md'>
+                                <Icon as={FaInstagramSquare} background={'#6A45BE'} w={6} h={6} me={4}/> 
+                                <Text>Offers</Text>
+                            </HStack>
+
+                            <HStack mt={8} size='md'>
+                                <Icon as={FaInstagramSquare} background={'#6A45BE'} w={6} h={6} me={4}/> 
+                                <Text>Contact</Text>
+                            </HStack>
+                            
+                        </DrawerBody>
+
+                        <DrawerFooter>
+                      
+                        </DrawerFooter>
+                        <DrawerCloseButton
+                            color={'#070707'}
+                            borderRadius={'100%'}
+                            background={'#F4C600'}
+                            position={'fixed'}
+                            top={'unset'}
+                            right={'50%'}
+                            transform={'translate(50%,0%)'}
+                            bottom={12} w={14} h={14}>
+                            <CloseIcon w={8} h={8}/>
+                        </DrawerCloseButton>
+                    </DrawerContent>
+                </Drawer>
                 <Box me={6} flexGrow={1} pt={1} w={'100%'} h={'100%'}>
                     <Image
                         src={img1} alt='poster' 
