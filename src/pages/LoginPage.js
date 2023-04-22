b2107566becd73c822a54049f2dbf537be5a160c
dawidektr <40436561+dawidektr@users.noreply.github.com>
2023-04-22 20:17:23 +0200

    Register And Login Style
    Style Changed

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -5,35 +5,35 @@ import { Link } from 'react-router-dom';
 export const LoginPage = () => {
     return (
         <>
-            <Box mt={8}>
-                <Text textAlign={'left'} color={'#FCFCFC'} size="lg">
+            <Box mt={8} color={'#F4C600'}>
+                <Text textAlign={'left'}  size="lg">
             Log into your account
                 </Text>
                 <HStack pt={6}>
-                    <Button w={'100%'}>Facebook</Button>
-                    <Button w={'100%'}>Google</Button>
+                    <Button variant={'base'}  w={'100%'}>Facebook</Button>
+                    <Button variant={'base'} w={'100%'}>Google</Button>
                 </HStack>
-                <Text pt={6} color={'#FCFCFC'} size="lg">
+                <Text pt={6}  size="lg">
             OR
                 </Text>
                 <FormControl pt={4}>
                     <Input color={'#FCFCFC'}  border={'none'} type='email'  background='#181C25' placeholder='Enter your Email' />   
                 </FormControl>
                 <FormControl mt={4}>  
-                    <Input color={'#FCFCFC'}  border={'none'} type='password'  background='#181C25' placeholder='Enter your Password' />   
+                    <Input color={'#FCFCFC'}   border={'none'} type='password'  background='#181C25' placeholder='Enter your Password' />   
                 </FormControl>
                 <FormControl mt={5}>  
-                    <Checkbox color={'#FCFCFC'} w={'100%'} defaultChecked="false">Remember me</Checkbox>
+                    <Checkbox  w={'100%'} defaultChecked="false">Remember me</Checkbox>
                 </FormControl>
                 <FormControl mt={5}>  
-                    <Checkbox color={'#FCFCFC'} w={'100%'} defaultChecked="false">Accept the website Terms and Conditions.</Checkbox>
+                    <Checkbox  w={'100%'} defaultChecked="false">Accept the website Terms and Conditions.</Checkbox>
                 </FormControl>
-                <Button mt={6} borderRadius={50} w={'100%'}>Log into</Button>
+                <Button variant={'base'} mt={6} borderRadius={50} w={'100%'}>Log into</Button>
                 
-                <Text mt={2} width={'100%'} color={'#FCFCFC'} size="xs">
+                <Text mt={4} width={'100%'}  size="sm">
                 Are you new? Creating an account is quick.<br/>
-                    <Link to={'/login'}>Create an account</Link>
                 </Text>
+                <Link to={'/register'}><Button variant={'base'} mt={6}>Create an account</Button></Link>
                 
             </Box>
 
--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -5,15 +5,15 @@ import { Link } from 'react-router-dom';
 export const RegisterPage = () => {
     return (
         <>
-            <Box mt={8}>
-                <Text textAlign={'left'} color={'#FCFCFC'} size="lg">
+            <Box mt={8} color={'#F4C600'}>
+                <Text textAlign={'left'}  size="lg">
             Register account
                 </Text>
                 <HStack pt={6}>
-                    <Button w={'100%'}>Facebook</Button>
-                    <Button w={'100%'}>Google</Button>
+                    <Button variant={'base'} w={'100%'}>Facebook</Button>
+                    <Button  variant={'base'} w={'100%'}>Google</Button>
                 </HStack>
-                <Text pt={6} color={'#FCFCFC'} size="lg">
+                <Text pt={6}  size="lg">
             OR
                 </Text>
                 <FormControl pt={4}>
@@ -23,20 +23,21 @@ export const RegisterPage = () => {
                     <Input color={'#FCFCFC'}  border={'none'} type='password'  background='#181C25' placeholder='Enter your Password' />   
                 </FormControl>
                 <FormControl mt={4}>  
-                    <Input color={'#FCFCFC'}  border={'none'} type='password'  background='#181C25' placeholder='Repeat your Password' />   
+                    <Input   border={'none'} type='password'  background='#181C25' placeholder='Repeat your Password' />   
                 </FormControl>
                 <FormControl mt={5}>  
-                    <Checkbox color={'#FCFCFC'} w={'100%'} defaultChecked="false">Remember me</Checkbox>
+                    <Checkbox  w={'100%'} defaultChecked="false">Remember me</Checkbox>
                 </FormControl>
                 <FormControl mt={5}>  
-                    <Checkbox color={'#FCFCFC'} w={'100%'} defaultChecked="false">Accept the website Terms and Conditions.</Checkbox>
+                    <Checkbox  w={'100%'} defaultChecked="false">Accept the website Terms and Conditions.</Checkbox>
                 </FormControl>
-                <Button mt={6} borderRadius={50} w={'100%'}>Register</Button>
+                <Button variant={'base'} mt={6} borderRadius={50} w={'100%'}>Register</Button>
                 
-                <Text mt={2} width={'100%'} color={'#FCFCFC'} size="xs">
+                <Text mt={4} width={'100%'}  size="sm">
                 Are you with us already? Log into your account.<br/>
-                    <Link to={'/register'} color='gray'>Log in</Link>
+                   
                 </Text>
+                <Link to={'/login'} color='gray'><Button variant={'base'} mt={6}>Log in</Button></Link>
                 
             </Box>
 
